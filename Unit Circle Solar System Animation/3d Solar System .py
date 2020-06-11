e24bcc1c9dc28071589d94276eea04c0f7ae53e4
WilliamMass <wamass@pdx.edu>
2020-06-11 15:35:39 -0700

    Added data for Gliese 581 system.
    Refactoring and changes to Solar System - Basic bools allow for either 'Sol' or 'Gliese' systems to be simulated.
    - Gliese 581 system can be

---
+++ Unit Circle Solar System Animation/3d Solar System .py
@@ -0,0 +1,143 @@
+from SolarClasses import Planet, Star
+import turtle
+import time
+import numpy as np
+
+simulation = True
+
+'''
+Any system is able to be simulated, but only these sets of values are included which provide a (somewhat) realistic
+simulation of actual planetary systems.  Please note that 'Sol' is not actually the name of the Sun, I just enjoy
+science-fiction naming conventions.  All these things are initialized below in the if statements.  In order to change
+the system being plotted, simply comment/uncomment the appropriate line.
+'''
+system = 'Sol'
+# system = 'Gliese'
+normalize_ratios = False  # This is only checked if the system is not our own Solar System
+
+window = turtle.Screen()
+window.setup(width=1.0, height=1.0, startx=0, starty=0)
+window.bgcolor('black')
+window.tracer(0)
+
+'''
+300 pixels is, somewhat arbitrarily, equivalent to 1 AU (96 million miles, the distance from the earth to the sun).
+This allows us to develop accurate ratios for the distances of the other planets from the sun.   Because the size of the
+window is fixed to be the size of the screen on which the simulation is running, Jupiter may or may not be wholly
+visible.  All the planets are drawn to scale as closely as possible.  All stars are NOT drawn to scale.
+'''
+
+if system == 'Sol':
+    sun = Star(5, 'gold')
+    earth = Planet(300, 'mediumblue', 1, sun, 100)
+    mercury = Planet(117, 'burlywood', 0.38, sun, 0)
+    venus = Planet(217, 'magenta', 0.95, sun, 50)
+    mars = Planet(452, 'firebrick', .53, sun, 100)
+    jupiter = Planet(1509, 'orange', 11.2, sun, 100)
+
+    '''
+    These lunar distances are quite inaccurate - in reality, the moon would have a distance of about .9, and each of
+    the moons of mars would be somewhere around .011 and .039 respectively.  But that makes the plotting not look
+    particularly good, so the 'Rule of Cool' was followed.
+    '''
+    moon = Planet(20, 'grey', 0.27, earth, 0)
+    phobos = Planet(19, 'gainsboro', 0.11, mars, 3)
+    deimos = Planet(19, 'light slate grey', 0.0981, mars, 0)
+
+    asteroids = True
+
+    bodies = [earth, mercury, venus, mars, jupiter, moon, phobos, deimos]
+    for body in bodies:
+        body.penup()
+        body.goto(body.radius + body.offset, 0)
+        if body.star == sun:
+            body.pendown()
+
+elif system == 'Gliese':
+    '''
+    The planets in Gliese are in proximal order to Gliese 581, so closest to furthest.  Their names are alphabetical in
+    order of their discovery. - The distances here (the radius/semimajor axis value defined when each Planet object is
+    created) are 20% of what they are in our solar system.  For instance, Gliese 581e (defined ge, below) is .02815 of
+    an astronomical unit from its star; plotting that in Turtle would make it draw its orbit essentially completely
+    within its star, so the values have been scaled instead.  Once again, all Planets are scaled correctly, porportional
+    to the size of Earth, and the star is _not_ drawn to realistic scale.
+    '''
+    g581 = Star(3, 'orangered')
+    ge = Planet(168, 'tomato', 1.2, g581, 1)  # These are in star-major order, i.e. closest to farthest from the star.
+    gb = Planet(242, 'chocolate', 3.8, g581, 1)
+    gc = Planet(432, 'thistle', 1.4, g581, 10)
+    gd = Planet(1260, 'deepskyblue', 2.2, g581, -180)
+
+    '''
+     The system is known to have a massive system of icy planetoids at its edge, similar but denser than the Kuiper Belt
+     at the edge of our Solar System - but it is far beyond the distance able to be rendered by Turtle's ability to draw
+     things around a fixed point on the screen!  So, to save processor cycles, we'll just turn asteroids off.
+    '''
+    asteroids = False
+
+    bodies = [ge, gb, gc, gd]
+    for body in bodies:
+        body.penup()
+        body.goto(body.radius + body.offset, 0)
+        if body.star == g581:
+            body.pendown()
+
+asteroid_belt = []
+angle = 0.001
+
+if asteroids:
+    for i in range(525):
+        asteroid = Planet(np.random.randint(555, 900), 'peru', 0.1, sun, 100)
+        asteroid.penup()
+        asteroid_belt.append(asteroid)
+        asteroid.angle += angle
+        angle += 0.0214121
+
+while simulation:
+    window.update()
+    for body in bodies:
+        body.x_y_offset_motion()
+
+    '''
+    For each time slice, the position is increased by 0.0x radians, as based on the ratio of the planets orbital period
+    to that of the Earth. In other words, over the time steps it takes for earth to complete an orbit in Turtle the
+    angle is increased by .01.  In that same period of time, the angle for mercury is increased by 0.0414, since Mercury
+    takes 88 days to complete one orbit of the sun.
+    '''
+    if system == 'Sol':
+        moon.angle += 0.12
+        phobos.angle += 0.6
+        deimos.angle += 0.4
+
+        mercury.angle += 0.0414
+        venus.angle += 0.0162
+        earth.angle += 0.01
+        mars.angle += 0.005335
+        jupiter.angle += 0.00084315
+
+    '''
+    For each time slice, the position is increased by 0.0x radians, as based on the ratio of the planets orbital period
+    to that of the Gliese 581d.  This is the "outermost" planet in this system, which is right at the edge of the star's
+    "Goldilocks Zone" and could potentially support life.  All of these planets have _much_ faster orbits than any of
+    the planets in our Solar System - Gliese 581e completes one orbit in 3.149 Earth days!  The comments contain values
+    with the ratios expressed as ratios of Earth's orbital period as well, so that the speed difference can be compared to that of our solar
+    system.
+    '''
+    if system == 'Gliese':
+        if normalize_ratios:
+            ge.angle += 1.159  # (Ratio to Gliese 581d: 0.46956 - Ratio to Earth: 1.159)
+            gb.angle += 0.6798  # (Ratio to Gliese 581d: 0.12455 - Ratio to Earth: 0.6798)
+            gc.angle += 0.2993  # (Ratio to Gliese 581d: 0.05178 - Ratio to Earth: 0.2993)
+            gd.angle += 0.055  # (If using Ratio to self for other calculations: 0.01 - Ratio to Earth: 0.055)
+
+        else:
+            ge.angle += 0.46956
+            gb.angle += 0.12455
+            gc.angle += 0.05178
+            gd.angle += 0.01
+
+    for i in asteroid_belt:
+        i.x_y_offset_motion()
+        i.angle += 0.002
+
+    time.sleep(0.01)
--- Unit Circle Solar System Animation/SolarClasses.py
+++ Unit Circle Solar System Animation/SolarClasses.py
@@ -3,9 +3,10 @@ from math import *
 
 
 class Star(turtle.Turtle):
-    def __init__(self, shapesize_x, shapesize_y, color):
+    def __init__(self, size, color):
         super().__init__(shape='circle')
-        self.shapesize = (shapesize_x, shapesize_y)
+        self.size = size
+        self.shapesize(size, size)
         self.c = color
         self.color(self.c)
 
@@ -13,7 +14,7 @@ class Star(turtle.Turtle):
 class Planet(turtle.Turtle):
     def __init__(self, radius, color, size, star, offset=0):
         super().__init__(shape='circle')
-        self.radius = radius
+        self.radius = radius  # this is really equivalent to the semi-major axis... but everything is a circle anyway
         self.c = color
         self.color(self.c)
         self.size = size
@@ -23,16 +24,25 @@ class Planet(turtle.Turtle):
         self.star = star
         self.offset = offset
         self.draw = 'on'
-        self.ellipse = 0.3
+        self.ellipse = 0.3  # Altering this could draw more accurate orbits; but it's easier to just use n-body calcs?
 
+    '''
+    This is the measurement of the angle in radians - whenever this is called, the object's perceived angle on the unit
+    circle, as compared to the x and y axes in Python is recalculated.
+    '''
     def x_y_motion(self):
-        x = self.radius*cos(self.angle)  # Measurement of angle in radians
+        x = self.radius*cos(self.angle)
         y = self.radius*sin(self.angle)
 
         self.goto(self.star.xcor()+x, self.star.ycor()+y)
 
+    '''
+    The addition of an offset on the x value "simulates" a three-dimensional plot in 2 dimensions, by foreshortening the
+    orbits.  The offsets are individually defined when the planet is instantiated, with zero representing a circular
+    orbit.
+    '''
     def x_y_offset_motion(self):
         x = self.offset + self.radius*cos(self.angle)
-        y = self.radius*sin(self.angle)*0.3
+        y = self.radius*sin(self.angle)*(self.ellipse)
 
         self.goto(self.star.xcor()+x, self.star.ycor()+y)
